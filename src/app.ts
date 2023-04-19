c2a3e870c78027754b0b7768fbd2b7a805f72722
RubsRafa <rubiarafaela2000@gmail.com>
2023-04-19 12:22:57 -0300

    feat: add jest and jest config

--- src/app.ts
+++ src/app.ts
@@ -14,3 +14,5 @@ app.use(handleErrors)
 const port = process.env.PORT || 5001;
 
 app.listen(port, () => console.log(`Server running on port ${port}`))
+
+export default app;
---
+++ tests/itens.test.ts
@@ -0,0 +1,9 @@
+import { any } from "joi";
+import app from "../src/app.js";
+import itensControllers from "../src/controllers/itensControllers.js";
+
+describe ('GET /itens', () => {
+    it('should respond with all itens', async () => {
+        console.log('entrou no teste')   
+    })
+})
